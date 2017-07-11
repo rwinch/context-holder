ca39ca13d3e35ea20372d4de7ac04b989fb7d5a0
Rob Winch <rwinch@users.noreply.github.com>
2017-07-11 15:33:33 -0500

    Use WebTestClient (#1)

--- src/test/java/com/example/context/AppTests.java
+++ src/test/java/com/example/context/AppTests.java
@@ -2,23 +2,31 @@ package com.example.context;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AppTests {
+	@Autowired
+	ApplicationContext context;
 
 	@Test
-	public void contextLoads() {
+	public void contextHolderWorks() {
 
-		WebClient.create()
-		         .get()
-		         .uri("http://localhost:8080/foo/1")
-		         .retrieve()
-		         .bodyToMono(String.class)
-		         .block();
+		WebTestClient client = WebTestClient.bindToApplicationContext(context)
+				.build();
+
+		client.get()
+				.uri("/foos/1")
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody(String.class).consumeWith( response -> assertThat(response.getResponseBody()).contains("Joe"));
 	}
 
 }
